7f63338145de2b956d6545be8b68a1ba7cbfa815
Waqas Tahir <wakaztahir@gmail.com>
2024-06-11 16:08:00 +0500

    added IDE release configurations

---
+++ release.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Scripts required to build the project are broken at the moment
+
+#"D:/Software/JetBrains/CLion 2023.3.4/bin/cmake/win/x64/bin/cmake.exe" -DCMAKE_C_COMPILER="cl.exe" -DCMAKE_CXX_COMPILER="cl.exe" -DCMAKE_BUILD_TYPE=Release -S D:/chemical -B D:/chemical/out/build/x64-release --fresh
+
+#"D:/Software/JetBrains/CLion 2023.3.4/bin/cmake/win/x64/bin/cmake.exe" --build D:/chemical/out/build/x64-release
+
+# Assembling Release from already built package
+

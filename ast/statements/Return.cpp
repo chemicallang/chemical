1800e7d313eee5e254bac83f9cb7d154f9e50d88
Waqas Tahir <wakaztahir@gmail.com>
2024-10-04 20:35:42 +0500

    all return values are checked against return types

--- ast/statements/Return.cpp
+++ ast/statements/Return.cpp
@@ -34,12 +34,11 @@ void ReturnStatement::declare_and_link(SymbolResolver &linker) {
             }
             const auto implicit = func_type->returnType->implicit_constructor_for(linker.allocator, value);
             if (implicit && implicit != func_type && implicit->parent_node != func_type->parent()) {
-//                if(linker.preprocess) {
-//                    value = call_with_arg(implicit, value, linker);
-//                } else {
-                    link_with_implicit_constructor(implicit, linker, value);
-//                }
+                link_with_implicit_constructor(implicit, linker, value);
                 return;
+            }
+            if(!func_type->returnType->satisfies(linker.allocator, value)) {
+                linker.unsatisfied_type_err(value, func_type->returnType);
             }
         }
     }

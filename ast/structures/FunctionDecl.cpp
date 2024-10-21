979ce0383654322d08f2bf26d817b1b048043712
Waqas Tahir <wakaztahir@gmail.com>
2024-10-21 15:53:01 +0500

    check that no function exists with same name before inserting in the module

--- ast/structures/FunctionDecl.cpp
+++ ast/structures/FunctionDecl.cpp
@@ -354,6 +354,12 @@ std::string FunctionDeclaration::runtime_name_fast(Codegen& gen) {
 }
 
 void create_non_generic_fn(Codegen& gen, FunctionDeclaration *decl, const std::string& name) {
+#ifdef DEBUG
+    auto existing_func = gen.module->getFunction(name);
+    if(existing_func) {
+        gen.error("function with name '" + name + "' already exists in the module", (ASTNode*) decl);
+    }
+#endif
     auto func_type = decl->create_llvm_func_type(gen);
     auto func = gen.create_function(name, func_type, decl->specifier);
     llvm_func_def_attr(func);

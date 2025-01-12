add16a78544e061409ca2a8ae36bb82aa335ba40
Waqas Tahir <wakaztahir@gmail.com>
2025-01-12 18:45:54 +0500

    number values always try to get integer type from the linked type

--- ast/values/NumberValue.cpp
+++ ast/values/NumberValue.cpp
@@ -10,17 +10,18 @@
 #include "ast/values/IntValue.h"
 #include "compiler/SymbolResolver.h"
 
+IntNType* get_int_n_type(BaseType* type) {
+    if(!type) return nullptr;
+    const auto pure = type->pure_type();
+    return pure->as_intn_type();
+}
+
 unsigned int NumberValue::get_num_bits() {
     if(linked_type == nullptr) {
         return 32;
     } else {
-        const auto pure = linked_type->pure_type();
-        const auto num_type = pure->as_intn_type();
-        if(num_type) {
-            return num_type->num_bits();
-        } else {
-            return 32;
-        }
+        const auto t = get_int_n_type(linked_type);
+        return t ? t->num_bits() : 32;
     }
 }
 
@@ -28,52 +29,28 @@ bool NumberValue::is_unsigned() {
     if(linked_type == nullptr) {
         return false;
     } else {
-        const auto pure = linked_type->pure_type();
-        const auto num_type = pure->as_intn_type();
-        if(num_type) {
-            return num_type->is_unsigned();
-        } else {
-            return false;
-        }
+        const auto t = get_int_n_type(linked_type);
+        return t != nullptr && t->is_unsigned();
     }
 }
 
 Value* NumberValue::evaluated_value(InterpretScope &scope) {
     if(linked_type) {
-        const auto pure = linked_type->pure_type();
-        const auto num_type = pure->as_intn_type();
-        if(num_type) {
-            return num_type->create(scope.allocator, value);
-        } else {
-            return this;
-        }
+        const auto t = get_int_n_type(linked_type);
+        return t ? t->create(scope.allocator, value) : this;
     } else {
         return this;
     }
 }
 
 ValueType NumberValue::value_type() const {
-    if(linked_type == nullptr) {
-        return ValueType::Int;
-    } else {
-        return linked_type->value_type();
-    }
+    const auto t = get_int_n_type(linked_type);
+    return t ? t->value_type() : ValueType::Int;
 }
 
 BaseType* NumberValue::known_type() {
-    if(!linked_type) {
-        return (BaseType*) &IntType::instance;
-    }
-    return linked_type;
-}
-
-IntNType* linked(BaseType* type) {
-    auto pure = type->pure_type();
-    if(pure && pure->kind() == BaseTypeKind::IntN) {
-        return (IntNType*) pure;
-    } else {
-        return nullptr;
-    }
+    const auto t = get_int_n_type(linked_type);
+    return t ? t : (BaseType*) &IntType::instance;
 }
 
 bool NumberValue::link(SymbolResolver &linker, BaseType *type) {
@@ -81,9 +58,11 @@ bool NumberValue::link(SymbolResolver &linker, BaseType *type) {
         const auto linked = type->linked_node();
         if(linked) {
             const auto param = linked->as_generic_type_param();
-            if(param && param->active_iteration < 0) {
-                if(param->def_type) {
-                    linked_type = param->def_type;
+            if(param) {
+                if(param->active_iteration < 0) {
+                    linked_type = type->copy(*linker.ast_allocator);
+                } else {
+                    linked_type = param->known_type()->copy(*linker.ast_allocator);
                 }
                 return true;
             }

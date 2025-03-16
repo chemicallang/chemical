cc8b5ac28b4060144acbbf791a1a02b988c101aa
Waqas Tahir <wakaztahir@gmail.com>
2025-03-16 12:56:41 +0500

    doesn't make use of NumberValue

--- parser/utils/LexValue.cpp
+++ parser/utils/LexValue.cpp
@@ -421,6 +421,18 @@ ValueNode* Parser::parseValueNode(ASTAllocator& allocator) {
 
 const auto unk_bit_width_err = "unknown bit width given for a number";
 
+Value* allocate_number_value(ASTAllocator& alloc, unsigned long long value, bool is64Bit, SourceLocation location) {
+    if(value > INT_MAX) {
+        if(value > LONG_MAX) {
+            return new(alloc.allocate<BigIntValue>()) BigIntValue((long long) value, location);
+        } else {
+            return new(alloc.allocate<LongValue>()) LongValue((long) value, is64Bit, location);
+        }
+    } else {
+        return new (alloc.allocate<IntValue>()) IntValue((int) value, location);
+    }
+}
+
 parse_num_result<Value*> convert_number_to_value(ASTAllocator& alloc, char* mut_value, std::size_t value_size, bool is64Bit, SourceLocation location) {
 
     // considers bitwidths
@@ -619,7 +631,7 @@ parse_num_result<Value*> convert_number_to_value(ASTAllocator& alloc, char* mut_
                 return { new(alloc.allocate<DoubleValue>()) DoubleValue(num_value.result, location), err.empty() ? num_value.error : err };
             } else {
                 const auto num_value = parse_num(value, value_size, strtoull);
-                return { new(alloc.allocate<NumberValue>()) NumberValue(num_value.result, location), err.empty() ? num_value.error : err };
+                return { allocate_number_value(alloc, num_value.result, is64Bit, location), err.empty() ? num_value.error : err };
             }
         }
     }

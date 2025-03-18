6accf4c7f06a9156388180c818c5ad1054abf76e
Waqas Tahir <wakaztahir@gmail.com>
2025-03-18 12:47:31 +0500

    tiny change

--- ast/structures/MembersContainer.cpp
+++ ast/structures/MembersContainer.cpp
@@ -638,16 +638,6 @@ FunctionDeclaration* MembersContainer::create_def_copy_fn(ASTAllocator& allocato
     return create_copy_fn(allocator);
 }
 
-template<typename T>
-void do_it(int a, int b) {
-
-}
-
-template<typename T>
-void do_it(int a, int b, int c) {
-
-}
-
 bool MembersContainer::insert_multi_func(ASTAllocator& astAllocator, FunctionDeclaration* decl) {
     auto found = indexes.find(decl->name_view());
     if(found == indexes.end()) {

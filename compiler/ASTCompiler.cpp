ef66a07f891efc9df03ee658d7c7ef6e5cd9932d
Waqas Tahir <wakaztahir@gmail.com>
2025-09-29 13:31:54 +0500

    version update

--- compiler/ASTCompiler.cpp
+++ compiler/ASTCompiler.cpp
@@ -11,7 +11,6 @@ void ASTProcessor::code_gen_declare(
         std::vector<ASTNode*>& nodes_vec,
         const std::string_view& abs_path
 ) {
-    CP_UTF8;
     std::unique_ptr<BenchmarkResults> bm_results;
     if(options->benchmark) {
         bm_results = std::make_unique<BenchmarkResults>();

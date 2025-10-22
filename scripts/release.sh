1616c02233fdc60cfaa25b98a13d7e7d5b0d821c
Waqas Tahir <wakaztahir@gmail.com>
2025-10-22 16:38:09 +0500

    macos added to workflow, although untested

--- scripts/release.sh
+++ scripts/release.sh
@@ -56,6 +56,10 @@ linux_lsp=false
 # Alpine Target
 alpine=false
 alpine_tcc=false
+# MacOS Target
+macos=false
+macos_tcc=false
+macos_lsp=false
 
 # Command line parameter variables
 zip_all_at_end=true
@@ -78,6 +82,10 @@ for param in "$@"; do
     elif [ "$param" = "--alpine" ]; then
           alpine=true
           alpine_tcc=true
+    elif [ "$param" = "--macos" ]; then
+          macos=true
+          macos_tcc=true
+          macos_lsp=true
     fi
 done
 
@@ -90,30 +98,39 @@ echo "linux_tcc: $linux_tcc"
 echo "linux_lsp: $linux_tcc"
 echo "alpine: $alpine"
 echo "alpine_tcc: $alpine_tcc"
+echo "macos: $macos"
+echo "macos_tcc: $macos_tcc"
+echo "macos_lsp: $macos_lsp"
 
 # Create directory names
 windows_dir_name="windows"
 linux_dir_name="linux"
 alpine_dir_name="linux-alpine"
+macos_dir_name="macos"
 # Tcc directory names
 windows_tcc_dir_name="$windows_dir_name-tcc"
 linux_tcc_dir_name="$linux_dir_name-tcc"
 alpine_tcc_dir_name="$alpine_dir_name-tcc"
+macos_tcc_dir_name="$macos_dir_name-tcc"
 # LSP directory names
 windows_lsp_dir_name="$windows_dir_name-lsp"
 linux_lsp_dir_name="$linux_dir_name-lsp"
+macos_lsp_dir_name="$macos_dir_name-lsp"
 
 # Create directory paths
 windows_dir="out/release/$windows_dir_name"
 linux_dir="out/release/$linux_dir_name"
 alpine_dir="out/release/$alpine_dir_name"
+macos_dir="out/release/$macos_dir_name"
 # Tcc directory paths
 windows_tcc_dir="out/release/$windows_tcc_dir_name"
 linux_tcc_dir="out/release/$linux_tcc_dir_name"
 alpine_tcc_dir="out/release/$alpine_tcc_dir_name"
+macos_tcc_dir="out/release/$macos_tcc_dir_name"
 # LSP directory paths
 windows_lsp_dir="out/release/$windows_lsp_dir_name"
 linux_lsp_dir="out/release/$linux_lsp_dir_name"
+macos_lsp_dir="out/release/$macos_lsp_dir_name"
 
 # Make directories required for each target
 if [ "$windows" = true ]; then
@@ -125,20 +142,29 @@ fi
 if [ "$alpine" = true ]; then
     mkdir -p "$alpine_dir/packages/tcc"
 fi
+if [ "$macos" = true ]; then
+    mkdir -p "$macos_dir/packages/tcc"
+fi
 if [ "$windows_tcc" = true ]; then
     mkdir -p "$windows_tcc_dir/packages/tcc"
-fi
-if [ "$alpine_tcc" = true ]; then
-    mkdir -p "$alpine_tcc_dir/packages/tcc"
 fi
 if [ "$linux_tcc" = true ]; then
     mkdir -p "$linux_tcc_dir/packages/tcc"
 fi
+if [ "$alpine_tcc" = true ]; then
+    mkdir -p "$alpine_tcc_dir/packages/tcc"
+fi
+if [ "$macos_tcc" = true ]; then
+    mkdir -p "$macos_tcc_dir/packages/tcc"
+fi
 if [ "$windows_lsp" = true ]; then
     mkdir -p "$windows_lsp_dir/packages/tcc"
 fi
-if [ "$linux_tcc" = true ]; then
-    mkdir -p "$linux_lsp_dir/packages/tcc"
+if [ "$linux_lsp" = true ]; then
+    mkdir -p "$linux_lsp_dir/packages/lsp"
+fi
+if [ "$macos_lsp" = true ]; then
+    mkdir -p "$macos_lsp_dir/packages/lsp"
 fi
 
 # -------------------------- windows
@@ -184,6 +210,21 @@ if [ "$alpine" = true ]; then
   cp lib/tcc/libtcc.so "$alpine_dir/libtcc.so"
   cp -r lib/tcc/include "$alpine_dir/packages/tcc"
   cp -r lib/tcc/lib "$alpine_dir/packages/tcc"
+fi
+
+# -------------------------- macos
+
+if [ "$macos" = true ]; then
+  # copy compiler
+  cp out/build/Compiler "$macos_dir/chemical"
+  # copy resources
+  cp -r ./lib/include "$macos_dir/resources"
+  # copy the libs directory
+  cp -r lang/libs "$macos_dir/"
+  # copy the tinycc package
+  cp lib/tcc/libtcc.so "$macos_dir/libtcc.so"
+  cp -r lib/tcc/include "$macos_dir/packages/tcc"
+  cp -r lib/tcc/lib "$macos_dir/packages/tcc"
 fi
 
 # -------------------------- windows tcc
@@ -232,6 +273,22 @@ if [ "$alpine_tcc" = true ]; then
   cp -r lib/tcc/lib "$alpine_tcc_dir/packages/tcc"
 fi
 
+# -------------------------- macos tcc
+
+if [ "$macos_tcc" = true ]; then
+  # copy tcc compiler
+  cp out/build/TCCCompiler "$macos_tcc_dir/chemical"
+  # copy resources (tcc build doesn't need resources)
+  # cp -r ./lib/include "$macos_tcc_dir/resources"
+  # copy tiny cc dll
+  # copy the libs directory
+  cp -r lang/libs "$macos_tcc_dir/"
+  # copy the tinycc package
+  cp lib/tcc/libtcc.so "$macos_tcc_dir/libtcc.so"
+  cp -r lib/tcc/include "$macos_tcc_dir/packages/tcc"
+  cp -r lib/tcc/lib "$macos_tcc_dir/packages/tcc"
+fi
+
 # -------------------------- windows lsp
 
 if [ "$windows_lsp" = true ]; then
@@ -262,6 +319,21 @@ if [ "$linux_lsp" = true ]; then
   cp -r lib/tcc/lib "$linux_lsp_dir/packages/tcc"
 fi
 
+# -------------------------- macos lsp
+
+if [ "$macos_lsp" = true ]; then
+  # copy lsp
+  cp out/build/ChemicalLsp "$macos_lsp_dir/lsp"
+  # copy resources (tcc build doesn't need resources)
+  # cp -r ./lib/include "$macos_lsp_dir/resources"
+  # copy the libs directory
+  cp -r lang/libs "$macos_lsp_dir/"
+  # copy the tinycc package
+  cp lib/tcc/libtcc.so "$macos_lsp_dir/libtcc.so"
+  cp -r lib/tcc/include "$macos_lsp_dir/packages/tcc"
+  cp -r lib/tcc/lib "$macos_lsp_dir/packages/tcc"
+fi
+
 # ------------------------- done
 
 if [ "$zip_all_at_end" = true ]; then
@@ -276,6 +348,9 @@ if [ "$zip_all_at_end" = true ]; then
     if [ "$alpine" = true ]; then
       zip_folder "$alpine_dir_name" "linux-alpine.zip"
     fi
+    if [ "$macos" = true ]; then
+      zip_folder "$macos_dir_name" "macos.zip"
+    fi
     if [ "$windows_tcc" = true ]; then
       zip_folder "$windows_tcc_dir_name" "windows-tcc.zip"
     fi
@@ -285,11 +360,17 @@ if [ "$zip_all_at_end" = true ]; then
     if [ "$alpine_tcc" = true ]; then
       zip_folder "$alpine_tcc_dir_name" "linux-alpine-tcc.zip"
     fi
+    if [ "$macos_tcc" = true ]; then
+      zip_folder "$macos_tcc_dir_name" "macos-tcc.zip"
+    fi
     if [ "$windows_lsp" = true ]; then
       zip_folder "$windows_lsp_dir_name" "windows-lsp.zip"
     fi
     if [ "$linux_lsp" = true ]; then
       zip_folder "$linux_lsp_dir_name" "linux-lsp.zip"
+    fi
+    if [ "$macos_lsp" = true ]; then
+      zip_folder "$macos_lsp_dir_name" "macos-lsp.zip"
     fi
     cd ../../
 fi
@@ -305,6 +386,9 @@ if [ "$delete_dirs_at_end" = true ]; then
     if [ "$alpine" = true ]; then
       rm -rf "$alpine_dir"
     fi
+    if [ "$macos" = true ]; then
+      rm -rf "$macos_dir"
+    fi
     if [ "$windows_tcc" = true ]; then
       rm -rf "$windows_tcc_dir"
     fi
@@ -314,10 +398,16 @@ if [ "$delete_dirs_at_end" = true ]; then
     if [ "$alpine_tcc" = true ]; then
       rm -rf "$alpine_tcc_dir"
     fi
+    if [ "$macos_tcc" = true ]; then
+      rm -rf "$macos_tcc_dir"
+    fi
     if [ "$windows_lsp" = true ]; then
       rm -rf "$windows_lsp_dir"
     fi
     if [ "$linux_lsp" = true ]; then
       rm -rf "$linux_lsp_dir"
+    fi
+    if [ "$macos_lsp" = true ]; then
+      rm -rf "$macos_lsp_dir"
     fi
 fi
